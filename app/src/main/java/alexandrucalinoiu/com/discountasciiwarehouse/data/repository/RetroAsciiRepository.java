a198f7b5ebb4ade27f401fb036bb84802737e655
calin <calinoiu.alexandru@agilefreaks.com>
2015-10-17 07:23:43 +0300

    adds caching

--- app/src/main/java/alexandrucalinoiu/com/discountasciiwarehouse/data/repository/RetroAsciiRepository.java
+++ app/src/main/java/alexandrucalinoiu/com/discountasciiwarehouse/data/repository/RetroAsciiRepository.java
@@ -1,7 +1,13 @@
 package alexandrucalinoiu.com.discountasciiwarehouse.data.repository;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import com.squareup.okhttp.Cache;
 import com.squareup.okhttp.OkHttpClient;
 
+import java.io.File;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -10,6 +16,7 @@ import javax.inject.Singleton;
 import alexandrucalinoiu.com.discountasciiwarehouse.data.converter.AsciiResponseConverter;
 import alexandrucalinoiu.com.discountasciiwarehouse.domain.model.Ascii;
 import alexandrucalinoiu.com.discountasciiwarehouse.domain.model.QueryParams;
+import retrofit.RequestInterceptor;
 import retrofit.RestAdapter;
 import retrofit.client.OkClient;
 import retrofit.http.GET;
@@ -21,6 +28,7 @@ import rx.Observable;
 public class RetroAsciiRepository implements AsciiRepository {
 
   private final AsciiService asciiService;
+  private Context context;
 
   public interface AsciiService {
     @Headers({
@@ -31,17 +39,51 @@ public class RetroAsciiRepository implements AsciiRepository {
   }
 
   @Inject
-  public RetroAsciiRepository() {
-    OkHttpClient client = new OkHttpClient();
+  public RetroAsciiRepository(Context context) {
+    this.context = context;
 
     RestAdapter retrofit = new RestAdapter.Builder()
         .setEndpoint("http://74.50.59.155:5000/api")
-        .setClient(new OkClient(client))
+        .setClient(new OkClient(buildOkClient()))
+        .setRequestInterceptor(buildRequestInterceptor())
         .setLogLevel(RestAdapter.LogLevel.FULL)
         .setConverter(new AsciiResponseConverter())
         .build();
 
     asciiService = retrofit.create(AsciiService.class);
+  }
+
+  private RequestInterceptor buildRequestInterceptor() {
+    return new RequestInterceptor() {
+      @Override
+      public void intercept(RequestInterceptor.RequestFacade request) {
+        request.addHeader("Content-Type", "application/json; charset=utf8");
+        if (isNetworkAvailable(context)) {
+          int maxAge = 0;
+          request.addHeader("Cache-Control", "public, max-age=" + maxAge);
+        } else {
+          int maxStale = 60;
+          request.addHeader("Cache-Control", "public, only-if-cached, max-stale=" + maxStale);
+        }
+      }
+    };
+  }
+
+  private boolean isNetworkAvailable(Context context) {
+    ConnectivityManager cm =
+        (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+    NetworkInfo netInfo = cm.getActiveNetworkInfo();
+    return netInfo != null && netInfo.isConnectedOrConnecting();
+  }
+
+  private OkHttpClient buildOkClient() {
+    OkHttpClient client = new OkHttpClient();
+    File httpCacheDirectory = new File(context.getCacheDir(), "responses");
+    Cache cache = new Cache(httpCacheDirectory, 10 * 1024 * 1024);
+
+    client.setCache(cache);
+
+    return client;
   }
 
   public Observable<List<Ascii>> search(QueryParams params) {
